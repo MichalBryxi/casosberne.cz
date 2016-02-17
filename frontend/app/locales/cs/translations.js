4919e40eafe9b70d73a704c7620b8b9ad15503db
Michal Bryxí <michal.bryxi@gmail.com>
2016-02-17 07:39:38 +0100

    Some layout and i18n things

---
+++ frontend/app/locales/cs/translations.js
@@ -0,0 +1,25 @@
+export default {
+  menu: {
+    home: 'Domů',
+    projects: 'Projekty',
+    contact: 'Kontakt'
+  },
+
+  index: {
+    heading: 'Tajm&mdash;Lapster',
+
+    1: {
+      heading: 'Vyfoť',
+      message: 'Pomocí stojanu udělej stejnou unifikovanou fotku.'
+    }
+  }
+  // "some.translation.key": "Text for some.translation.key",
+  //
+  // "a": {
+  //   "nested": {
+  //     "key": "Text for a.nested.key"
+  //   }
+  // },
+  //
+  // "key.with.interpolation": "Text with {{anInterpolation}}"
+};
--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,6 +6,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('contact');
+  this.route('projects');
 });
 
 export default Router;
