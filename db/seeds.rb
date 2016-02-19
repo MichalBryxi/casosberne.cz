78a8d338a395db5eba29599bbbcdec53074f9bca
Michal Bryxí <michal.bryxi@gmail.com>
2016-02-19 07:48:29 +0100

    Basic API for projects implemented

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+projects = Project.create([
+  {
+    name: 'Křížek u Vejprnic',
+    description: 'Foo bar',
+    hashtag: 'plzenCasosberne',
+  }
+])
