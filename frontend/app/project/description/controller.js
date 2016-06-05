1029b72cffd9643e2f094fd55dae0be0c372832e
Michal Bryxí <michal.bryxi@gmail.com>
2016-06-05 10:36:42 +0100

    JSCS fixes & removed unused lodash

--- frontend/app/project/description/controller.js
+++ frontend/app/project/description/controller.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import { linkify } from 'ember-linkify/helpers/linkify';
 import moment from 'moment';
-import _ from 'lodash/lodash';
 
 export default Ember.Controller.extend({
   i18n: Ember.inject.service(),
@@ -10,7 +9,7 @@ export default Ember.Controller.extend({
   postsSorted: Ember.computed.sort('model.posts', 'sortBy'),
   defaultImage: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/A_blank_flag.png',
 
-  photoSwipePosts: Ember.computed.map('postsSorted', function (post) {
+  photoSwipePosts: Ember.computed.map('postsSorted', function(post) {
     let date = moment(post.get('createdAt')).format('DD.MM.');
     let body = linkify([post.getWithDefault('body', '')]);
     let user = post.getWithDefault('user', '');
@@ -20,7 +19,7 @@ export default Ember.Controller.extend({
     let w = 600;
     let h = 338;
     let src = post.getWithDefault('image');
-    if(Ember.isEmpty(src)) {
+    if (Ember.isEmpty(src)) {
       src = this.get('defaultImage');
     }
     let createdAt = post.get('createdAt');
