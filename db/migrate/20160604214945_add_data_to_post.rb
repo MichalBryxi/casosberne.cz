d39fa37a0e32f37c7fd67d379a82fe8f3a4f9ae3
Michal Bryxí <michal.bryxi@gmail.com>
2016-06-05 11:23:17 +0100

    Cleaned up migrations, my fault

--- db/migrate/20160604214945_add_data_to_post.rb
+++ db/migrate/20160604214945_add_data_to_post.rb
@@ -2,9 +2,5 @@ class AddDataToPost < ActiveRecord::Migration
   def change
     add_column :posts, :payload, :json, null: true
     add_column :posts, :user, :string, null: true
-    remove_column :posts, :image_file_name
-    remove_column :posts, :image_content_type
-    remove_column :posts, :image_file_size
-    remove_column :posts, :image_updated_at
   end
 end
