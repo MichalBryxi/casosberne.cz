51708b777f29b2e4fd7bbba9ff3c0fa9289025e8
Michal Bryxí <michal.bryxi@gmail.com>
2016-06-05 09:31:54 +0100

    Shait!

--- frontend/app/project/description/controller.js
+++ frontend/app/project/description/controller.js
@@ -7,29 +7,22 @@ export default Ember.Controller.extend({
   i18n: Ember.inject.service(),
   sortBy: ['createdAt:desc'],
   isFormDisplayed: false,
-  photoSwipePosts: [],
   postsSorted: Ember.computed.sort('model.posts', 'sortBy'),
+  defaultImage: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/A_blank_flag.png',
 
-  // PhotoSwipe internally rewrites data of whatever is returned here.
-  // Ember does not like it's data being modified outside of .set() methods.
-  // A little hacky solution, but works.
-  postsObserver: Ember.observer('postsSorted.[]', function () {
-    let photoSwipeData = this.get('postsSorted').map(function (post) {
-      let date = moment(post.get('createdAt')).format('DD.MM.');
-      let body = linkify([post.getWithDefault('body', '')]);
-      let user = post.getWithDefault('user', '');
-      let userUrl = `<a href="http://twitter.com/${user}">@${user}</a>`;
+  photoSwipePosts: Ember.computed.map('postsSorted', function (post) {
+    let date = moment(post.get('createdAt')).format('DD.MM.');
+    let body = linkify([post.getWithDefault('body', '')]);
+    let user = post.getWithDefault('user', '');
+    let userUrl = `<a href="http://twitter.com/${user}">@${user}</a>`;
 
-      let title = `${date} - ${body} - ${userUrl}`;
-      let w = 600;
-      let h = 338;
-      let src = post.get('image');
-      let createdAt = post.get('createdAt');
+    let title = `${date} - ${body} - ${userUrl}`;
+    let w = 600;
+    let h = 338;
+    let src = post.getWithDefault('image', this.get('defaultImage'));
+    let createdAt = post.get('createdAt');
 
-      return { src, w, h, title, createdAt };
-    });
-
-    this.set('photoSwipePosts', photoSwipeData);
+    return { src, w, h, title, createdAt };
   }),
 
   actions: {
