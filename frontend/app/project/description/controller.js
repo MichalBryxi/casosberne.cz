b39d1c9bc6408cd250c9d3cf590777badddca598
Michal Bryxí <michal.bryxi@gmail.com>
2016-06-05 08:20:17 +0100

    Checkpoind - right after I broke it :)

--- frontend/app/project/description/controller.js
+++ frontend/app/project/description/controller.js
@@ -1,25 +1,35 @@
 import Ember from 'ember';
 import { linkify } from 'ember-linkify/helpers/linkify';
 import moment from 'moment';
+import _ from 'lodash/lodash';
 
 export default Ember.Controller.extend({
   i18n: Ember.inject.service(),
   sortBy: ['createdAt:desc'],
   isFormDisplayed: false,
-
+  photoSwipePosts: [],
   postsSorted: Ember.computed.sort('model.posts', 'sortBy'),
 
-  posts: Ember.computed.map('postsSorted', function(post) {
-    let date = moment(post.get('createdAt')).format('DD.MM.');
-    let body = linkify([post.getWithDefault('body', '')]);
+  // PhotoSwipe internally rewrites data of whatever is returned here.
+  // Ember does not like it's data being modified outside of .set() methods.
+  // A little hacky solution, but works.
+  postsObserver: Ember.observer('postsSorted.[]', function () {
+    let photoSwipeData = this.get('postsSorted').map(function (post) {
+      let date = moment(post.get('createdAt')).format('DD.MM.');
+      let body = linkify([post.getWithDefault('body', '')]);
+      let user = post.getWithDefault('user', '');
+      let userUrl = `<a href="http://twitter.com/${user}">@${user}</a>`;
 
-    let title = `${date} - ${body}`;
-    let w = 600;
-    let h = 338;
-    let src = post.get('image');
-    let createdAt = post.get('createdAt');
+      let title = `${date} - ${body} - ${userUrl}`;
+      let w = 600;
+      let h = 338;
+      let src = post.get('image');
+      let createdAt = post.get('createdAt');
 
-    return { src, w, h, title, createdAt };
+      return { src, w, h, title, createdAt };
+    });
+
+    this.set('photoSwipePosts', photoSwipeData);
   }),
 
   actions: {
